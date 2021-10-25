4af3d824ec93679afa0334db0c798c737055a498
unknown <juanpabloferroarroyo@gmail.com>
2021-10-25 06:01:10 +0200

    implementing the form with the components Form, Field & ErrorMessage provided by Formik

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const validate = (values) => {
   const errors = {};
@@ -18,36 +18,27 @@ const validate = (values) => {
 };
 
 function App() {
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      lastname: "",
-      email: "",
-    },
-    validate,
-    onSubmit: (values) => console.log(values),
-  });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>Name</label>
-      <input type="text" {...formik.getFieldProps("name")} />
-      {formik.touched.name && formik.errors.name ? (
-        <div>{formik.errors.name}</div>
-      ) : null}
-      <br />
-      <label>LastName</label>
-      <input type="text" {...formik.getFieldProps("lastname")} />
-      {formik.touched.lastname && formik.errors.lastname ? (
-        <div>{formik.errors.lastname}</div>
-      ) : null}
-      <br />
-      <label>Email</label>
-      <input type="email" {...formik.getFieldProps("email")} />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
-      <button type="submit">Send</button>
-    </form>
+    <Formik
+      initialValues={{ name: "", lastname: "", email: "" }}
+      validate={validate}
+      onSubmit={(values) => console.log(values)}
+    >
+      <Form>
+        <label>Name</label>
+        <Field name="name" type="text" />
+        <ErrorMessage name="name" />
+        <br />
+        <label>LastName</label>
+        <Field name="lastname" type="text" />
+        <ErrorMessage name="lastname" />
+        <br />
+        <label>Email</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
+        <button type="submit">Send</button>
+      </Form>
+    </Formik>
   );
 }
 
